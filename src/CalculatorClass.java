ccb93d8dd84abd97d7ee4f9e7c00b3b5332af8d2
kailaakailaa <kailabullard@yahoo.com>
2020-12-27 22:38:36 -0600

    First commit

---
+++ src/CalculatorClass.java
@@ -0,0 +1,37 @@
+
+public class CalculatorClass {
+	int num1;
+	int num2;
+	
+public static int add(int num1, int num2) {
+	
+	return num1 + num2;
+}
+public static int subtract(int num1, int num2) {
+	
+	return num1 - num2;
+}
+public static int multiply(int num1, int num2) {
+	
+	return num1 * num2;
+}
+public static int divide(int num1, int num2) {
+	
+	return num1 / num2;
+}
+public static int square(int num1, int num2) {
+	
+	return (int)Math.sqrt(num1);
+	
+} public static void  MagicCalculator(){
+	
+	
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(multiply(3,5));
+	}
+	
+
+}
